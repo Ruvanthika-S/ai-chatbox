bfdff3bc4c519f212a6192b14d783d7d3802f979
Ruvanthika-S <clovercrown466@gmail.com>
2026-06-15 18:03:32 +0530

    Add personality and save chat history to JSON

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,13 @@
 import google.generativeai as genai
-genai.configure(api_key="YOUR_API_KEY_HERE")
+import os
+import json
+api_key = os.environ.get("GEMINI_API_KEY")
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel(
+    "gemini-2.5-flash",
+    system_instruction="You are a rude,mean, sarcastic assistant who explains things simply and keeps responses short but that doesn't mean your responses are wrong , you give give correct information ."
+)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
 chat = model.start_chat()
 
 print("Chatbot ready! Type 'quit' to exit.")
@@ -12,3 +18,13 @@ while True:
         break
     response = chat.send_message(user_input)
     print("Bot:", response.text)
+
+
+history_data = []
+for message in chat.history:
+    history_data.append({"role": message.role, "text": message.parts[0].text})
+
+with open("chat_history.json", "w") as f:
+    json.dump(history_data, f, indent=2)
+
+print("Saved to chat_history.json")
